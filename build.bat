9cbe27af8b4160c1c385ef676a9349dcc69a39b6
Kroc Camen <kroc@camendesign.com>
2023-03-13 09:21:16 +0000

    Build two versions
    Default and a C64OS version.
    Added a "build" folder;
    build artefacts now go there.
    Various tidying readying for a releasable version.

--- build.bat
+++ build.bat
@@ -4,10 +4,22 @@ PUSHD %~dp0
 
 SET ACME="bin\acme\acme.exe"
 
+REM # build default BSOD64 at $C000
 %ACME% -v ^
-    -l "bsod64.sym" ^
+    -o "build\bsod64.prg" ^
+    -l "build\bsod64.sym" ^
     --format cbm ^
      -Wtype-mismatch ^
+      "bsod64.acme"
+
+REM # build a version for C64OS during boot
+%ACME% -v ^
+    -o "build\bsod64-c64os.prg" ^
+    -l "build\bsod64-c64os.sym" ^
+    --format cbm ^
+     -Wtype-mismatch ^
+     -DBSOD64_CODE_ADDR=$7c80 ^
+     -DBSOD64_DATA_ADDR=$7000 ^
       "bsod64.acme"
 
 POPD
