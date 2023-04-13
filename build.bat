9c4901be478050e50bc2881459dfc2b52fd266d2
Kroc Camen <kroc@camendesign.com>
2023-04-13 14:20:53 +0100

    Build a disk image
    Puts BSOD64 & BSOD64-C64OS onto a disk image to help with tranferring the binaries to a C64 OS installation

--- build.bat
+++ build.bat
@@ -4,6 +4,9 @@ PUSHD %~dp0
 
 SET ACME="bin\acme\acme.exe"
 
+REM # VICE's c1541 tool is used to create disk images
+SET C1541="bin\vice\c1541.exe"
+
 REM # build default BSOD64 at $C000
 %ACME% -v ^
     -o "build\bsod64.prg" ^
@@ -20,5 +23,11 @@ REM # build a version for C64OS during boot
      -DBSOD64_DATA_ADDR=$8000 ^
      -DBSOD64_USE_BRK=0 ^
       "bsod64.acme"
+
+REM # build a disk image:
+%C1541% ^
+    -format "bsod64","2a" d64 "build/bsod64.d64" ^
+    -write  "build/bsod64.prg"        "bsod64" ^
+    -write  "build/bsod64-c64os.prg"  "bsod64-c64os"
 
 POPD
